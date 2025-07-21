9ad9fcfc68111745d2337989a209d22ba2f5e9c3
AshtonSW <sprauncy76@gmail.com>
2025-07-21 19:39:17 +0900

    [chore] 프로젝트 초기 세팅

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './shared/assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+//test
+import './shared/assets/tailwind.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
