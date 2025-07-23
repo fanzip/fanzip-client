1962079e1acf5bd5567473449b972f96c655a4ad
AshtonSW <sprauncy76@gmail.com>
2025-07-24 07:13:32 +0900

    [fix] 네비게이션 바를 w-full + max-w로 변경하여 다양한 디바이스에서 중앙 정렬 문제 해결

--- src/components/layout/AppNav.vue
+++ src/components/layout/AppNav.vue
@@ -27,33 +27,31 @@ const navItems = [
 const isActive = (path) => route.path === path
 </script>
 <template>
-    <nav
-      class="fixed bottom-0 left-1/2 -translate-x-1/2 w-[393px] h-[76px] bg-white
-             border-t border-l border-r border-[#EFEFEF] rounded-t-[16px] flex items-center justify-center z-50"
-    >
-      <div class="flex justify-between w-full px-[20px]">
-        <RouterLink
-          v-for="item in navItems"
-          :key="item.path"
-          :to="item.path"
-          class="flex flex-col items-center gap-[8px] w-[56px]"
-          :aria-label="item.name"
+  <nav
+    class="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[430px] h-[76px] bg-white border-t border-l border-r border-[#EFEFEF] rounded-t-[16px] flex items-center justify-center z-50"
+  >
+    <div class="flex justify-between w-full px-[20px]">
+      <RouterLink
+        v-for="item in navItems"
+        :key="item.path"
+        :to="item.path"
+        class="flex flex-col items-center gap-[8px] w-[56px]"
+        :aria-label="item.name"
+      >
+        <img
+          :src="isActive(item.path) ? item.iconActive : item.icon"
+          class="w-[24px] h-[24px]"
+          :alt="item.name"
+        />
+        <span
+          :class="[
+            'block text-[12px] leading-none text-center whitespace-nowrap transition-colors duration-200',
+            isActive(item.path) ? 'text-black' : 'text-[#A7A7A7]',
+          ]"
         >
-          <img
-            :src="isActive(item.path) ? item.iconActive : item.icon"
-            class="w-[24px] h-[24px]"
-            :alt="item.name"
-          />
-          <span
-            :class="[
-              'block text-[12px] leading-none text-center whitespace-nowrap transition-colors duration-200',
-              isActive(item.path) ? 'text-black' : 'text-[#A7A7A7]',
-            ]"
-          >
-            {{ item.name }}
-          </span>
-        </RouterLink>
-      </div>
-    </nav>
-  </template>
-  
+          {{ item.name }}
+        </span>
+      </RouterLink>
+    </div>
+  </nav>
+</template>
