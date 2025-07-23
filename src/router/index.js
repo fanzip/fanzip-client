aa70cffa13a11352f1efcefcbaf092531e8cb929
AshtonSW <sprauncy76@gmail.com>
2025-07-24 07:05:41 +0900

    [feat] 하단 네비게이션 바(AppNav.vue) 구현 및 경로별 연결 완료

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DesignGuidePage from '@/pages/DesignGuidePage.vue'
+import MarketPage from '@/pages/MarketPage.vue'
+import MyPage from '@/pages/MyPage.vue'
+import FanCardPage from '@/pages/FanCardPage.vue'
+import FanMeetingPage from '@/pages/FanMeetingPage.vue'
+import HomePage from '@/pages/HomePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', name: 'home', component: HomePage },
+    { path: '/reservation', name: 'reservation', component: FanMeetingPage },
+    { path: '/fancard', name: 'fancard', component: FanCardPage },
+    { path: '/market', name: 'market', component: MarketPage },
+    { path: '/mypage', name: 'mypage', component: MyPage },
+
     {
       path: '/guide',
       name: 'Guide',
@@ -15,7 +26,7 @@ const router = createRouter({
     //   name: 'home',
     //   component: HomeView,
     // },
-    // {
+    // {  
     //   path: '/about',
     //   name: 'about',
     //   // route level code-splitting
