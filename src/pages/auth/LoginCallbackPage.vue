00bcf799a9fd8dfdf29ffc5196405a98643a389d
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-28 21:19:25 +0900

    [feat] 로그인 콜백 처리 (LoginCallbackPage.vue)

---
+++ src/pages/auth/LoginCallbackPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useAuthStore } from '@/stores/authStore'
+
+const authStore = useAuthStore()
+
+onMounted(async () => {
+  const code = new URLSearchParams(window.location.search).get('code')
+  if (code) {
+    await authStore.handleKakaoLogin(code)
+  }
+})
+</script>
+<template>LoginCallbackPage</template>
