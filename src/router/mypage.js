b9762c191722ed138ec7f86e0705fca3885c3df4
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-30 14:09:58 +0900

    [feat] mypage 라우터 설정

--- src/router/mypage.js
+++ src/router/mypage.js
@@ -1,2 +1,10 @@
-import MyPage from '@/pages/MyPage.vue'
-export default [{ path: '/mypage', name: 'mypage', component: MyPage }]
+import MyAccount from '@/pages/mypage/MyAccount.vue'
+import MyPage from '@/pages/mypage/MyPage.vue'
+import MyPayments from '@/pages/mypage/MyPayments.vue'
+import MyShippingAddress from '@/pages/mypage/MyShippingAddress.vue'
+export default [
+  { path: '/mypage', name: 'mypage', component: MyPage },
+  { path: '/account', name: 'account', component: MyAccount },
+  { path: '/shipping', name: 'shipping', component: MyShippingAddress },
+  { path: '/my-payments', name: 'myPayments', component: MyPayments },
+]
