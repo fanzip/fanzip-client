5aa9b2755f85d260881a09cc2243e7eda62cc63f
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-30 14:20:12 +0900

    [refactor] authApi: export default로 수정

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,11 +1,19 @@
 import axios from 'axios'
+import api from '@/api'
 
-export async function getKakaoLoginUrl() {
-  const res = await axios.get(`/api/auth/oauth/kakao-url`)
-  return res.data
+const authApi = {
+  getKakaoLoginUrl: async () => {
+    const res = await axios.get(`/api/auth/oauth/kakao-url`)
+    return res.data
+  },
+  kakaoLogin: async (code) => {
+    const res = await axios.get(`/api/auth/oauth/kakao-login?code=${code}`)
+    return res
+  },
+  requestLogout: async () => {
+    const res = await api.post('/api/auth/logout')
+    return res
+  },
 }
 
-export async function kakaoLogin(code) {
-  const res = await axios.get(`/api/auth/oauth/kakao-login?code=${code}`)
-  return res
-}
+export default authApi
