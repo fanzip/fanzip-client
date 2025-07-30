71fe30f6ca8c42ac19aae3a01093a32ae94cdfa6
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-30 14:23:08 +0900

    [feat] ListItem.vue: 마이페이지 목록 컴포넌트

---
+++ src/components/myPage/ListItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  menu: {
+    type: String,
+    default: '',
+  },
+  gap: {
+    type: String,
+    default: '1', // Tailwind 기준 gap-1 기본값
+  },
+})
+</script>
+
+<template>
+  <div :class="`w-full flex gap-${gap} mt-4`">
+    <img :src="src" alt="" class="ms-5" />
+    <p class="font-semibold">{{ menu }}</p>
+  </div>
+</template>
