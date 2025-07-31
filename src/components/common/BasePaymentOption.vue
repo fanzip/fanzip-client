d4949d755f426408d7f08cae3b8a3a6066857769
joojinhyeok <jjh68979@gmail.com>
2025-07-31 21:30:14 +0900

    [feat] 결제 수단 선택 공통 컴포넌트(BasePaymentOption) 추가 #27

---
+++ src/components/common/BasePaymentOption.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  modelValue: String, // v-model용
+})
+defineEmits(['update:modelValue'])
+
+import KbPayIcon from '@/assets/membership/KB.svg'
+import KakaoPayIcon from '@/assets/membership/kakao.svg'
+import TossIcon from '@/assets/membership/toss.svg'
+
+const paymentMethods = [
+  { id: 'kb', name: 'KB Pay', icon: KbPayIcon },
+  { id: 'kakao', name: '카카오페이', icon: KakaoPayIcon },
+  { id: 'toss', name: '토스페이', icon: TossIcon },
+]
+</script>
+
+<template>
+  <div class="bg-white rounded-lg">
+    <h2 class="font-bold text-[16px] mb-4">결제 방법</h2>
+
+    <div
+      v-for="method in paymentMethods"
+      :key="method.id"
+      class="flex items-center justify-between px-4 py-3 border rounded-lg mb-2 cursor-pointer"
+      :class="modelValue === method.id ? 'border-yellow-400' : 'border-gray-300'"
+      @click="$emit('update:modelValue', method.id)"
+    >
+      <div class="flex items-center gap-3">
+        <div class="relative w-4 h-4 rounded-full border border-[#DEDEDE] flex-shrink-0">
+          <div
+            v-if="modelValue === method.id"
+            class="absolute top-1/2 left-1/2 w-[10px] h-[10px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#FFBC00]"
+          ></div>
+        </div>
+
+        <img :src="method.icon" alt="logo" class="w-6 h-6" />
+        <span class="font-medium">{{ method.name }}</span>
+      </div>
+    </div>
+  </div>
+</template>
