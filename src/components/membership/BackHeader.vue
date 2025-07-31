0d4f660ac0012829b0b6db23bf9c784a1935fce2
joojinhyeok <jjh68979@gmail.com>
2025-07-31 21:30:00 +0900

    [feat] 멤버십 페이지 뒤로가기 헤더 컴포넌트 구현 #27

---
+++ src/components/membership/BackHeader.vue
@@ -0,0 +1,13 @@
+<script setup>
+import BackIcon from '../../assets/membership/Vector (Stroke).svg' // 저장한 svg 경로에 맞게 수정
+const goBack = () => {
+  history.back()
+}
+</script>
+
+<template>
+  <div class="flex items-center w-full h-[48px] px-5 bg-white">
+    <img :src="BackIcon" alt="뒤로가기" class="w-6 h-6 cursor-pointer" @click="goBack" />
+    <slot />
+  </div>
+</template>
