b8dc645c9e258f58d53d30ed018cb6496b273573
khanz0613 <khanz0613@naver.com>
2025-08-01 14:27:16 +0900

    [feat] SearchBar 컴포넌트 스타일 정리 및 노출 기능 추가
    - Tailwind CSS 클래스 정리 및 일관성 개선
    - 검색어 키워드 외부 노출 기능 추가 (defineExpose)
    - 코드 포매팅 및 가독성 향상

--- src/components/common/SearchBar.vue
+++ src/components/common/SearchBar.vue
@@ -6,19 +6,19 @@
     </button>
 
     <!-- 배경색 고정: bg-subtle-bg (#F6F6F6), 크기 전환 없음 -->
-    <div class="relative flex items-center w-[353px] h-[32px] bg-subtle-bg rounded-[16px]">
+    <div class="relative flex items-center w-[353px] h-8 bg-subtle-bg rounded-2xl">
       <input
         v-model="keyword"
         :placeholder="placeholder"
-        class="w-full h-full text-[16px] placeholder-text-subtle-text rounded-[16px] outline-none bg-transparent pl-[16px] pr-[36px]"
+        class="w-full h-full text-base text-[#504E4E] rounded-2xl outline-none bg-transparent pl-4 pr-9"
       />
 
       <!-- 돋보기 아이콘 or X 버튼 -->
       <template v-if="!keyword">
-        <img src="@/assets/searchbar/search.svg" alt="검색" class="absolute right-[16px] w-4 h-4" />
+        <img src="@/assets/searchbar/search.svg" alt="검색" class="absolute right-4 w-4 h-4" />
       </template>
       <template v-else>
-        <button @click="clearKeyword" class="absolute right-[16px] w-4 h-4">
+        <button @click="clearKeyword" class="absolute right-4 w-4 h-4">
           <img src="@/assets/searchbar/xbutton.svg" alt="지우기" />
         </button>
       </template>
@@ -27,18 +27,16 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 
 const keyword = ref('')
-const placeholder = '검색어를 입력해주세요'
-
-const emit = defineEmits(['update:keyword'])
+const placeholder = '검색어를 입력해주세요.'
 
 const clearKeyword = () => {
   keyword.value = ''
 }
 
-watch(keyword, (val) => {
-  emit('update:keyword', val)
+defineExpose({
+  keyword,
 })
 </script>
