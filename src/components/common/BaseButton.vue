fab5c6ccbaf0ca5163e447c6de6384f432417652
dayeon1211 <dayeon1211m@gmail.com>
2025-08-01 14:30:55 +0900

    [style] 디자인 가이드에 맞춰 수정 #32

--- src/components/common/BaseButton.vue
+++ src/components/common/BaseButton.vue
@@ -19,22 +19,22 @@ const buttonClass = computed(() => {
     'font-semibold',
   ]
 
-  if (props.size === 'lg') base.push('w-[353px]', 'h-[48px]', 'text-[16px]')
-  if (props.size === 'md') base.push('w-[169px]', 'h-[48px]', 'text-[16px]')
-  if (props.size === 'sm') base.push('w-[140px]', 'h-[48px]', 'text-[16px]')
+  if (props.size === 'lg') base.push('w-[353px]', 'h-12', 'text-base')
+  if (props.size === 'md') base.push('w-[169px]', 'h-12', 'text-base')
+  if (props.size === 'sm') base.push('w-[140px]', 'h-12', 'text-base')
 
   switch (props.variant) {
     case 'primary':
       base.push('bg-brand-primary', 'text-black', 'hover:bg-brand-accent')
       break
     case 'cart':
-      base.push('bg-white', 'text-black', 'border-2', 'border-[#EAEAEA]', 'hover:bg-subtle-bg')
+      base.push('bg-base-bg', 'text-black', 'border-2', 'border-base-border', 'hover:bg-subtle-bg')
       break
     case 'buy':
       base.push('bg-brand-primary', 'text-black', 'hover:bg-brand-accent')
       break
     case 'cancel':
-      base.push('bg-[#EFEFEF]', 'text-black')
+      base.push('bg-nav-stroke', 'text-black')
       break
     case 'confirm':
       base.push('bg-brand-primary', 'text-black')
@@ -47,7 +47,7 @@ const buttonClass = computed(() => {
 
 <template>
   <button :type="type" :class="buttonClass">
-    <img v-if="icon" :src="icon" alt="icon" class="w-[20px] h-[20px]" />
+    <img v-if="icon" :src="icon" alt="icon" class="w-5 h-5" />
     <slot />
   </button>
 </template>
