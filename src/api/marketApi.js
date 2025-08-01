e684004ccf3cf67027d4af0044c4f1838cb1838a
dayeon1211 <dayeon1211m@gmail.com>
2025-08-01 14:30:33 +0900

    [refactor] market page api 분리 #32

--- src/api/marketApi.js
+++ src/api/marketApi.js
@@ -1,1 +1,16 @@
-// 공동구매 마켓 API
+import api from '@/api'
+
+const marketApi = {
+  getProducts: async ({ limit = 20, keyword = '', lastProductId = null }) => {
+    const params = { limit }
+    if (keyword) params.q = keyword
+    if (lastProductId) params.lastProductId = lastProductId
+
+    const res = await api.get('/api/market/products', { params })
+    console.log('res:', res)
+    console.log('res.data: ', res.data)
+    return res.data
+  },
+}
+
+export default marketApi
