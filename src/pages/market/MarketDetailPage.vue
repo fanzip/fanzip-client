8dac2ac2a8fb76fa8de9742a9a93a6c0063b630b
dayeon1211 <dayeon1211m@gmail.com>
2025-08-01 11:14:50 +0900

    [feat] 품절, 오픈 시간 안된 경우 화면 구현 #32

--- src/pages/market/MarketDetailPage.vue
+++ src/pages/market/MarketDetailPage.vue
@@ -62,7 +62,23 @@ function formatOpenTime(iso) {
   const dd = String(d.getDate()).padStart(2, '0')
   const hh = String(d.getHours()).padStart(2, '0')
   const mi = String(d.getMinutes()).padStart(2, '0')
-  return `${d.getFullYear()}년 ${MM}월 ${dd}일 ${hh}시 ${mi}분`
+  const ss = String(d.getSeconds()).padStart(2, '0')
+  return `${d.getFullYear()}년 ${MM}월 ${dd}일 ${hh}:${mi}:${ss}`
+}
+
+function getGradeName(g) {
+  switch (g) {
+    case 1:
+      return 'White'
+    case 2:
+      return 'Silver'
+    case 3:
+      return 'Gold'
+    case 4:
+      return 'Vip'
+    default:
+      return '일반'
+  }
 }
 
 async function addToCart() {
@@ -91,7 +107,6 @@ function goToPurchase() {
   <AppHeader type="back-icons" />
 
   <!-- 품절 & 오픈전 오버레이-->
-  <!-- <div class="relative"> -->
   <div
     v-if="product.stock === 0 || !product.available"
     class="fixed top-12 left-0 right-0 bottom-0 bg-black bg-opacity-30 z-10 pointer-events-none"
@@ -150,6 +165,38 @@ function goToPurchase() {
         alt="description"
       />
     </div>
+
+    <!-- 품절인 경우 -->
+    <div v-if="product.stock === 0">
+      <nav
+        class="fixed bottom-0 left-0 w-full h-20 bg-base-bg border-t border-nav-stroke rounded-t-2xl flex items-center justify-center z-50"
+      >
+        <div
+          class="w-[353px] h-12 bg-subtle-border rounded-xl font-bold text-base flex items-center justify-center"
+        >
+          품절
+        </div>
+      </nav>
+    </div>
+
+    <!-- 오픈 시간이 안된 경우 -->
+    <div v-else-if="!product.available">
+      <nav
+        class="fixed bottom-0 left-0 w-full h-28 bg-base-bg border-t border-nav-stroke rounded-t-2xl flex items-center justify-center z-50"
+      >
+        <div
+          class="w-[353px] h-16 bg-subtle-border rounded-xl text-base flex flex-col items-center justify-center text-center leading-snug font-semibold"
+        >
+          <span>
+            <span class="font-extrabold">{{ getGradeName(product.gradeId) }}</span> 등급의 오픈
+            시간은
+          </span>
+          <span>
+            <span class="font-extrabold">{{ formatOpenTime(product.openTime) }}</span
+            >입니다.
+          </span>
+        </div>
+      </nav>
+    </div>
   </div>
-  <!-- </div> -->
 </template>
