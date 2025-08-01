fe9a016a0717953ce3052584028fe0e4c4511fba
Hyewon Lee <hooho931@gmail.com>
2025-08-01 11:08:43 +0900

    [feat] payment 도메인 라우터 연결 #30

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,33 @@ import DesignGuidePage from '@/pages/DesignGuidePage.vue'
 import { reservationRoutes } from './reservation.js'
 
 import HomePage from '@/pages/HomePage.vue'
-import FanCardPage from '@/pages/fancard/FanCardPage.vue'
-import MarketPage from '@/pages/market/MarketPage.vue'
-import MyPage from '@/pages/mypage/MyPage.vue'
+import FanMeetingDetailPage from '@/pages/FanMeetingDetailPage.vue'
+
+import authRoutes from './auth'
+import fanMeetingRoutes from './fanMeeting'
+import membershipRoutes from './membership'
+import marketRoutes from './market'
+import mypageRoutes from './mypage'
+import cartRoutes from './cart'
+import fanCardRoutes from './fanCard'
+import paymentRoutes from './payment'
+
+const routes = [
+  // 기본 라우트
+  { path: '/', name: 'home', component: HomePage },
+  { path: '/guide', name: 'Guide', component: DesignGuidePage },
+  { path: '/reservation/:id', name: 'FanMeetingDetail', component: FanMeetingDetailPage },
+
+  // 도메인 라우트
+  ...authRoutes,
+  ...fanMeetingRoutes,
+  ...membershipRoutes,
+  ...fanCardRoutes,
+  ...marketRoutes,
+  ...mypageRoutes,
+  ...cartRoutes,
+  ...paymentRoutes
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior(to, from, savedPosition) {
