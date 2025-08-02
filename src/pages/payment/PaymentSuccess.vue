a08c10c9b9045e521a7c328e895f1a17b5fcc229
Hyewon Lee <hooho931@gmail.com>
2025-08-02 16:09:44 +0900

    [refactor] 결제 성공 페이지 Tailwind 클래스 적용 #30

--- src/pages/payment/PaymentSuccess.vue
+++ src/pages/payment/PaymentSuccess.vue
@@ -2,38 +2,35 @@
   <AppHeader type="back" />
 
   <!-- 로딩 중 -->
-  <div v-if="isLoading" class="flex flex-col items-center justify-center h-screen text-lg font-semibold">
+  <div v-if="isLoadinㅅg" class="flex flex-col items-center justify-center h-screen text-lg font-semibold">
     결제 승인 처리 중...
   </div>
 
   <!-- 결제 성공 -->
-  <div v-else-if="paymentData" class="flex flex-col items-center bg-white w-full h-screen">
+  <div v-else-if="paymentData" class="flex flex-col items-center base-bg w-full h-screen">
     <!-- 아이콘 -->
-    <div class="mt-64 w-20 h-20">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="white" stroke-width="2" viewBox="0 0 22 22" class="w-full h-full">
-        <circle cx="12" cy="12" r="11" fill="#FFD633" />
-        <path d="M7 12l3 3 7-7" stroke-linecap="round" stroke-linejoin="round" />
-      </svg>
-    </div>
+      <div class="mt-64 w-20 h-20">
+        <img :src="CheckSymbol" alt="체크" />
+      </div>
 
     <!-- 완료 메시지 -->
-    <p class="mt-[3.75rem] text-xl text-black font-normal">결제가 완료되었습니다!</p>
+    <p class="mt-14 text-xl text-base font-normal">결제가 완료되었습니다!</p>
 
     <!-- 버튼 -->
-    <div class="mt-[20.5625rem] px-5 w-full">
-      <BaseButton variant="primary" size="lg" @click="goToHome" class="w-full max-w-[22rem] mx-auto">
+    <div class="mt-80 absolute bottom-14">
+      <BaseButton variant="primary" size="lg" @click="goToHome">
         돌아가기
       </BaseButton>
     </div>
   </div>
 
   <!-- 에러 발생 -->
-  <div v-else class="flex flex-col items-center justify-center w-full h-screen bg-white text-center px-4">
-    <h1 class="text-[#dc3545] text-lg mt-25 font-semibold">
+  <div v-else class="flex flex-col items-center justify-center w-full h-screen base-bg text-center px-4">
+    <h1 class=" text-emphasis lg mt-25 font-semibold">
       {{ error || '결제 처리 중 오류가 발생했습니다' }}
     </h1>
      <div class="mt-10 px-5 w-full">
-       <BaseButton variant="primary" size="lg" @click="goToHome" class="w-full max-w-[22rem] mx-auto">
+       <BaseButton variant="primary" size="lg" @click="goToHome">
         돌아가기
       </BaseButton>
     </div>
@@ -45,6 +42,7 @@ import { ref, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import AppHeader from '@/components/layout/AppHeader.vue'
 import BaseButton from '@/components/common/BaseButton.vue'
+import CheckSymbol from '@/assets/icons/CheckSymbol.svg'
 
 export default {
   name: 'PaymentSuccess',
@@ -76,7 +74,7 @@ export default {
       isLoading.value = true
 
       try {
-        const approveResponse = await fetch(`http://localhost:8080/api/payments/${paymentId}/approve`, {
+        const approveResponse = await fetch(`/api/payments/${paymentId}/approve`, {
           method: 'PATCH',
           headers: {
             'Content-Type': 'application/json'
@@ -118,7 +116,8 @@ export default {
       paymentData,
       isLoading,
       error,
-      goToHome
+      goToHome,
+      CheckSymbol
     }
   }
 }
