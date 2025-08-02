28b7c68a6459fa33a012f4e4d4d6d280701147b4
Hyewon Lee <hooho931@gmail.com>
2025-08-02 18:12:24 +0900

    [refactor] index.js 충돌 해결

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DesignGuidePage from '@/pages/DesignGuidePage.vue'
-import { reservationRoutes } from './reservation.js'
-
 import HomePage from '@/pages/HomePage.vue'
-import FanMeetingDetailPage from '@/pages/FanMeetingDetailPage.vue'
+import DesignGuidePage from '@/pages/DesignGuidePage.vue'
+import FanCardPage from '@/pages/fancard/FanCardPage.vue'
+import MarketPage from '@/pages/market/MarketPage.vue'
+import MyPage from '@/pages/mypage/MyPage.vue'
+import FanMeetingDetailPage from '@/pages/reservation/FanMeetingDetailPage.vue'
 
 import authRoutes from './auth'
 import fanMeetingRoutes from './fanMeeting'
@@ -13,11 +14,15 @@ import mypageRoutes from './mypage'
 import cartRoutes from './cart'
 import fanCardRoutes from './fanCard'
 import paymentRoutes from './payment'
+import { reservationRoutes } from './reservation.js'
 
 const routes = [
   // 기본 라우트
   { path: '/', name: 'home', component: HomePage },
   { path: '/guide', name: 'Guide', component: DesignGuidePage },
+  { path: '/fancard', name: 'fancard', component: FanCardPage },
+  { path: '/market', name: 'market', component: MarketPage },
+  { path: '/mypage', name: 'mypage', component: MyPage },
   { path: '/reservation/:id', name: 'FanMeetingDetail', component: FanMeetingDetailPage },
 
   // 도메인 라우트
@@ -28,43 +33,16 @@ const routes = [
   ...marketRoutes,
   ...mypageRoutes,
   ...cartRoutes,
-  ...paymentRoutes
+  ...paymentRoutes,
+  ...reservationRoutes,
 ]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  scrollBehavior(to, from, savedPosition) {
-    // 항상 맨 위로 스크롤
+  routes,
+  scrollBehavior() {
     return { top: 0 }
   },
-  routes: [
-    { path: '/', name: 'home', component: HomePage },
-    { path: '/fancard', name: 'fancard', component: FanCardPage },
-    { path: '/market', name: 'market', component: MarketPage },
-    { path: '/mypage', name: 'mypage', component: MyPage },
-
-    {
-      path: '/guide',
-      name: 'Guide',
-      component: DesignGuidePage, // 👈 여기 추가
-    },
-
-    // 예약 관련 라우트들
-    ...reservationRoutes,
-
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
-  ],
 })
 
 export default router
