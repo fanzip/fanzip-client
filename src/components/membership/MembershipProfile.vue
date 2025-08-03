2e1dd37114e70a6e37f707b3b9adc70735070e14
joojinhyeok <jjh68979@gmail.com>
2025-08-03 22:22:16 +0900

    [chore] 인플루언서 svg파일 추가 및 png 파일 삭제

--- src/components/membership/MembershipProfile.vue
+++ src/components/membership/MembershipProfile.vue
@@ -1,32 +1,28 @@
 <script setup>
-import profileImage from '@/assets/membership/여단오.png'
-
-const profile = {
-  name: '여단오',
-  description: `안녕하세요 여단오입니다 저희는 한중커플입니다
-              2025/08/12 팬미팅에서 봐요~ヾ(^▽^*)))
-              2025/08/15 공구마켓 오픈 예정입니다.`,
-  imageUrl: profileImage,
-}
+defineProps({
+  name: String,
+  description: String,
+  imageUrl: String,
+})
 </script>
 
 <template>
-   <div class="flex items-start gap-4 bg-white p-5">
+  <div class="flex items-start gap-4 bg-white p-5">
     <!-- 프로필 이미지 -->
     <img
-      :src="profile.imageUrl"
-      :alt="`${profile.name} 프로필 이미지`"
+      :src="imageUrl"
+      :alt="`${name} 프로필 이미지`"
       class="w-20 h-20 rounded-full object-cover"
     />
 
     <!-- 텍스트 정보 -->
     <div>
       <!-- 이름 -->
-      <p class="text-lg font-bold mb-1">{{ profile.name }}</p>
+      <p class="text-lg font-bold mb-1">{{ name }}</p>
 
       <!-- 소개 텍스트 -->
       <p class="text-sm text-gray-700 leading-snug whitespace-pre-line">
-        {{ profile.description }}
+        {{ description }}
       </p>
     </div>
   </div>
