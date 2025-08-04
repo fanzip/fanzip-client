9671de49f0869b3a9bcd50359c7080f2e9835d94
AshtonSW <sprauncy76@gmail.com>
2025-08-04 23:50:54 +0900

    [feat] fanmeeting 메인 페이지 API 연동

---
+++ src/api/axios.js
@@ -0,0 +1,17 @@
+// 추후 필요시 주석 삭제
+// // api/axios.js
+// import axios from 'axios'
+
+// const instance = axios.create({
+//   baseURL: '/api',
+// })
+
+// instance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('accessToken')
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+
+// export default instance
