dcf24a805dd0c3682f6bd7bb549ec6b0049dd462
dayeon1211 <dayeon1211m@gmail.com>
2025-08-04 15:44:25 +0900

    [feat] api 추가 #36

--- src/api/marketApi.js
+++ src/api/marketApi.js
@@ -22,6 +22,29 @@ const marketApi = {
     const res = await api.post('/api/cart/items', { productId, quantity })
     return res.data
   },
+
+  // 장바구니 조회
+  getCartItems: async () => {
+    const res = await api.get('api/cart/items')
+    return res.data
+  },
+
+  // 개별 수량/선택 상태 변경
+  updateCartItem: async ({ cartItemId, quantity, isSelected }) => {
+    const res = await api.put(`/api/cart/items/${cartItemId}`, { quantity, isSelected })
+    return res.data
+  },
+
+  // 전체 선택/해제 토글
+  selectAll: async (selectAll) => {
+    const res = await api.put('/api/cart/items/select-all', {}, { params: { selectAll } })
+    return res.data
+  },
+
+  // 개별 삭제
+  removeCartItem: async (cartItemId) => {
+    await api.delete(`/api/cart/items/${cartItemId}`)
+  },
 }
 
 export default marketApi
