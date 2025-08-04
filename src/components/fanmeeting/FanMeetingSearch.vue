464c28aed6bebcb91c158660f838b35e2587c78d
AshtonSW <sprauncy76@gmail.com>
2025-08-04 23:50:41 +0900

    [feat] fanmeeting 메인 페이지 컴포넌트 구현 및 리팩토링 (#49)

---
+++ src/components/fanmeeting/FanMeetingSearch.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="pt-[68px] mb-7 flex justify-center">
+      <SearchBar ref="searchBarRef" />
+    </div>
+  </template>
+  
+  <script setup>
+  import SearchBar from '@/components/common/SearchBar.vue'
+  import { ref, defineExpose } from 'vue'
+  
+  const searchBarRef = ref(null)
+  
+  defineExpose({
+    searchBarRef,
+  })
+  </script>
+  
