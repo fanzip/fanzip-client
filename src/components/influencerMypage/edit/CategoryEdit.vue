70950ef5384b18f87d8be2a5c381be517732c10a
joojinhyeok <jjh68979@gmail.com>
2025-08-05 13:28:50 +0900

    [feat] 마이페이지 프로필 편집용 공통 컴포넌트 추가 #43

---
+++ src/components/influencerMypage/edit/CategoryEdit.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from 'vue'
+import { useInfluencerStore } from '@/stores/influencerStore'
+
+const categories = [
+  '뷰티',
+  '게임',
+  '일상',
+  '패션',
+  '요리',
+  '다이어트/건강',
+  '반려동물',
+  '키즈',
+  '교육/지식',
+  '여행',
+  '음악',
+  '운동',
+  '스포츠',
+  '언어',
+  '기타',
+]
+
+const influencerStore = useInfluencerStore()
+const selectedCategory = ref(influencerStore.category || '') // 초기값 설정
+
+const selectCategory = (category) => {
+  selectedCategory.value = category
+}
+
+defineExpose({ selectedCategory })
+</script>
+
+<template>
+  <div class="w-full px-5">
+    <div class="text-base font-bold mb-6">카테고리를 선택해주세요</div>
+
+    <div class="grid grid-cols-3 gap-2">
+      <button
+        v-for="category in categories"
+        :key="category"
+        @click="selectCategory(category)"
+        :class="[
+          'py-2 px-3 rounded-lg text-sm border text-center',
+          selectedCategory === category
+            ? 'text-base border-brand-accent font-semibold'
+            : 'bg-base-bg text-base border-base-border',
+        ]"
+      >
+        {{ category }}
+      </button>
+    </div>
+  </div>
+</template>
---
+++ src/components/influencerMypage/edit/FanCardEdit.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+
+const fileInputRef = ref(null)
+const selectedImage = ref(null)
+
+const onFileChange = (e) => {
+  const file = e.target.files[0]
+  if (file) {
+    selectedImage.value = URL.createObjectURL(file)
+  }
+}
+
+const triggerUpload = () => {
+  fileInputRef.value?.click()
+}
+
+defineExpose({
+  selectedImage,
+})
+</script>
+
+<template>
+  <div class="w-full px-5 flex flex-col items-start">
+    <div class="text-base font-bold mb-4 self-start">팬카드 사진 등록</div>
+    <div
+      class="w-full h-48 rounded-xl bg-subtle-bg border subtle-border flex items-center justify-center relative overflow-hidden"
+    >
+      <img
+        v-if="selectedImage"
+        :src="selectedImage"
+        alt="선택한 이미지"
+        class="absolute w-full h-full object-cover"
+      />
+      <button
+        v-else
+        class="flex items-center justify-center text-sm text-subtle-text bg-base-bg border border subtle-border px-4 py-2 rounded shadow"
+        @click="triggerUpload"
+      >
+        팬카드 이미지 등록
+      </button>
+      <input
+        type="file"
+        accept="image/*"
+        class="hidden"
+        ref="fileInputRef"
+        @change="onFileChange"
+      />
+    </div>
+  </div>
+</template>
