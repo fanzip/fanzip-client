b25e41acd3aba9e8cc8851670dbd9add8f57a98c
joojinhyeok <jjh68979@gmail.com>
2025-08-05 13:32:07 +0900

    [feat] 인플루언서 프로필 컴포넌트 추가 #43

---
+++ src/components/influencerMypage/profile/ProfileFanCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import { useInfluencerStore } from '@/stores/influencerStore'
+import Icon from '@/assets/membership/Vector.svg'
+import defaultFanCardImage from '@/assets/influencer/찰스엔터팬카드.svg'
+
+const router = useRouter()
+const influencerStore = useInfluencerStore()
+
+const goToEditFanCard = () => {
+  router.push('/influencer/profile/edit-fancard')
+}
+</script>
+
+<template>
+  <div class="px-5 mt-6 mb-6 space-y-2">
+    <!-- 타이틀 + 아이콘 -->
+    <div class="flex items-center justify-between cursor-pointer" @click="goToEditFanCard">
+      <span class="text-sm font-medium text-base">팬카드 이미지</span>
+      <img :src="Icon" alt=">" class="w-3 h-3 rotate-180" />
+    </div>
+
+    <!-- 이미지 박스 -->
+    <div class="w-full h-56 overflow-hidden rounded-md border border-subtle-border">
+      <img
+        :src="influencerStore.fanCardImage || defaultFanCardImage"
+        class="w-full h-full object-cover"
+        alt="팬카드 이미지"
+      />
+    </div>
+  </div>
+</template>
