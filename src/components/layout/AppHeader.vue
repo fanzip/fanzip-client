33a32e3647ad9608914cd8b457bdb9e246139ce1
dayeon1211 <dayeon1211m@gmail.com>
2025-08-05 17:17:21 +0900

    [fix] 로고 수정 #48

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -24,7 +24,9 @@ const goToCart = () => {
 </script>
 
 <template>
-  <header class="fixed top-0 left-0 right-0 z-50 w-full h-12 flex items-center px-5 bg-base-bg">
+  <header
+    class="fixed top-0 left-0 right-0 z-50 w-full h-12 flex items-center px-5 bg-base-bg text-xl"
+  >
     <!-- Close 전용 -->
     <div v-if="type == 'close'" class="flex items-center">
       <img src="@/assets/header/Close.svg" alt="Close" class="cursor-pointer" />
@@ -36,14 +38,14 @@ const goToCart = () => {
     </div>
 
     <!-- Logo 전용 -->
-    <div v-else-if="type === 'logo'" class="flex items-center">
-      <img src="@/assets/header/LogoName.svg" alt="Logo" class="h-6" />
+    <div v-else-if="type === 'logo' || type.startsWith('logo-')" class="flex items-center">
+      <img src="@/assets/header/logo.svg" alt="Logo" class="h-6" />
     </div>
 
     <!-- 중앙 타이틀 -->
     <span
       v-if="type === 'back-title' || type === 'back-title-icons'"
-      class="absolute left-1/2 -translate-x-1/2 text-base font-semibold text-text-base"
+      class="absolute left-1/2 -translate-x-1/2 font-semibold text-text-base"
     >
       {{ title }}
     </span>
@@ -56,7 +58,17 @@ const goToCart = () => {
       <img
         src="@/assets/header/CartIcon.svg"
         alt="Cart"
-        class="cursor-pointer hover:opacity-80"
+        class="cursor-pointer hover:opacity-80 h-6 w-6"
+        @click="goToCart"
+      />
+    </div>
+
+    <!-- logo + 장바구니 -->
+    <div v-if="type === 'logo-icons'" class="flex items-center gap-2 ml-auto">
+      <img
+        src="@/assets/header/CartIcon.svg"
+        alt="Cart"
+        class="cursor-pointer hover:opacity-80 h-6 w-6"
         @click="goToCart"
       />
     </div>
