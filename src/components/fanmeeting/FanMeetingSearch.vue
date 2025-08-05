d155179670177b8bd148b78327c4d7615f08366f
AshtonSW <sprauncy76@gmail.com>
2025-08-05 09:34:09 +0900

    [refactor] 코드 정렬 안되어 있는 문제 수정

--- src/components/fanmeeting/FanMeetingSearch.vue
+++ src/components/fanmeeting/FanMeetingSearch.vue
@@ -1,17 +1,16 @@
 <template>
-    <div class="pt-[68px] mb-7 flex justify-center">
-      <SearchBar ref="searchBarRef" />
-    </div>
-  </template>
-  
-  <script setup>
-  import SearchBar from '@/components/common/SearchBar.vue'
-  import { ref, defineExpose } from 'vue'
-  
-  const searchBarRef = ref(null)
-  
-  defineExpose({
-    searchBarRef,
-  })
-  </script>
-  
+  <div class="pt-[68px] mb-7 flex justify-center">
+    <SearchBar ref="searchBarRef" />
+  </div>
+</template>
+
+<script setup>
+import SearchBar from '@/components/common/SearchBar.vue'
+import { ref, defineExpose } from 'vue'
+
+const searchBarRef = ref(null)
+
+defineExpose({
+  searchBarRef,
+})
+</script>
