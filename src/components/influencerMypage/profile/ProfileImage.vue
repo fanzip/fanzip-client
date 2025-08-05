b982ddb6b046429b0ff60aac7602b8524fff7d2a
joojinhyeok <jjh68979@gmail.com>
2025-08-05 13:32:07 +0900

    [feat] 인플루언서 프로필 컴포넌트 추가 #43

---
+++ src/components/influencerMypage/profile/ProfileImage.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+import Ellipse from '@/assets/InfluencerProfile/Ellipse.svg'
+import PlusIcon from '@/assets/InfluencerProfile/Plus.svg'
+
+import { useInfluencerStore } from '@/stores/influencerStore'
+const influencerStore = useInfluencerStore()
+
+const router = useRouter()
+
+const goToEditImage = () => {
+  router.push('/influencer/profile/edit-image')
+}
+</script>
+
+<template>
+  <div class="flex justify-center mt-24">
+    <div class="relative w-24 h-24">
+      <!-- 프로필 이미지 -->
+      <img
+        :src="influencerStore.profileImage || defaultImage"
+        alt="프로필 이미지"
+        class="w-24 h-24 rounded-full object-cover border"
+      />
+      <div class="absolute bottom-0 right-0 w-6 h-6 cursor-pointer" @click="goToEditImage">
+        <img :src="Ellipse" alt="버튼 배경" class="absolute w-full h-full" />
+        <img :src="PlusIcon" alt="플러스" class="absolute w-3 h-3 top-1.5 left-1.5" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/influencerMypage/profile/ProfileMenu.vue
@@ -0,0 +1,65 @@
+<script setup>
+import Icon from '@/assets/membership/Vector.svg'
+import { useRouter } from 'vue-router'
+import { useInfluencerStore } from '@/stores/influencerStore'
+
+const router = useRouter()
+const influencerStore = useInfluencerStore()
+
+const goToEditName = () => {
+  router.push('/influencer/profile/edit-name')
+}
+
+const goToEditCategory = () => {
+  router.push('/influencer/profile/edit-category')
+}
+
+const goToEditIntro = () => {
+  router.push('/influencer/profile/edit-intro')
+}
+</script>
+
+<template>
+  <div class="px-5 space-y-4 mt-8">
+    <!-- 이름 -->
+    <div class="flex items-center justify-between">
+      <span class="text-sm text-base">이름</span>
+      <div class="flex items-center gap-1">
+        <span class="text-sm text-base">{{ influencerStore.name }}</span>
+        <img
+          :src="Icon"
+          alt=">"
+          class="w-3 h-3 rotate-180 ml-2 cursor-pointer"
+          @click="goToEditName"
+        />
+      </div>
+    </div>
+
+    <!-- 카테고리 -->
+    <div class="flex items-center justify-between">
+      <span class="text-sm text-base">카테고리</span>
+      <div class="flex items-center gap-1">
+        <span class="text-sm text-base">{{ influencerStore.category }}</span>
+        <img
+          :src="Icon"
+          alt=">"
+          class="w-3 h-3 rotate-180 ml-2 cursor-pointer"
+          @click="goToEditCategory"
+        />
+      </div>
+    </div>
+
+    <!-- 소개글 -->
+    <div>
+      <div class="flex justify-between items-start mb-4">
+        <span class="text-sm text-base">소개글</span>
+        <img :src="Icon" alt=">" class="w-3 h-3 rotate-180 cursor-pointer" @click="goToEditIntro" />
+      </div>
+      <div
+        class="border border-subtle-border rounded-lg px-4 py-3 text-sm text-text-base h-36"
+      >
+        {{ influencerStore.description || '소개글이 없습니다.' }}
+      </div>
+    </div>
+  </div>
+</template>
