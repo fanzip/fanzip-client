47091abb7cc3ad0eed0269a5c22af1c593740d78
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-11 21:20:01 +0900

    [feat] auth.js: 온보딩 라우터 설정

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,24 +1,34 @@
 import LoginPage from '@/pages/auth/LoginPage.vue'
 import AdditionalInfoPage from '@/pages/auth/AdditionalInfoPage.vue'
 import LoginCallbackPage from '@/pages/auth/LoginCallbackPage.vue'
+import UserOnboarding from '@/pages/auth/UserOnboarding.vue'
+import { comma } from 'postcss/lib/list'
+import InfluencerOnboarding from '@/pages/auth/InfluencerOnboarding.vue'
 
 export default [
   {
     path: '/login',
     name: 'Login',
     component: LoginPage,
-    meta: { hideNavbar: true },
   },
   {
     path: '/signup/additional-info',
     name: 'AdditionalInfo',
     component: AdditionalInfoPage,
-    meta: { hideNavbar: true },
   },
   {
     path: '/oauth/kakao/callback',
     name: 'LoginCallback',
     component: LoginCallbackPage,
-    meta: { hideNavbar: true },
+  },
+  {
+    path: '/onboarding/user',
+    name: 'UserOnBoarding',
+    component: UserOnboarding,
+  },
+  {
+    path: '/onboarding/influencer',
+    name: 'InfluencerOnBoarding',
+    component: InfluencerOnboarding,
   },
 ]
