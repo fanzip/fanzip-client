8f9007da751d20436635abdf9a45be5492ee64bf
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-11 21:36:00 +0900

    [feat] 일반 사용자 가입시 온보딩 연결

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,9 +1,6 @@
 import LoginPage from '@/pages/auth/LoginPage.vue'
 import AdditionalInfoPage from '@/pages/auth/AdditionalInfoPage.vue'
 import LoginCallbackPage from '@/pages/auth/LoginCallbackPage.vue'
-import UserOnboarding from '@/pages/auth/UserOnboarding.vue'
-import { comma } from 'postcss/lib/list'
-import InfluencerOnboarding from '@/pages/auth/InfluencerOnboarding.vue'
 
 export default [
   {
@@ -20,15 +17,5 @@ export default [
     path: '/oauth/kakao/callback',
     name: 'LoginCallback',
     component: LoginCallbackPage,
-  },
-  {
-    path: '/onboarding/user',
-    name: 'UserOnBoarding',
-    component: UserOnboarding,
-  },
-  {
-    path: '/onboarding/influencer',
-    name: 'InfluencerOnBoarding',
-    component: InfluencerOnboarding,
   },
 ]
