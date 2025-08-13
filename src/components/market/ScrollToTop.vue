5eae0ed621b3f2df5e45616b9ae226858d04c524
dayeon1211 <dayeon1211m@gmail.com>
2025-08-14 00:47:27 +0900

    [style] 투명도로 상품을 가리지 않도록 수정 #86

--- src/components/market/ScrollToTop.vue
+++ src/components/market/ScrollToTop.vue
@@ -2,10 +2,10 @@
   <button
     v-if="visible"
     @click="scrollTop"
-    class="fixed bottom-24 right-0 transform -translate-x-1/2 w-10 h-10 bg-white rounded-full shadow-lg flex items-center justify-center hover:subtle-border transition"
+    class="fixed bottom-24 right-0 transform -translate-x-1/2 w-10 h-10 bg-white rounded-full bg-opacity-70 shadow-lg flex items-center justify-center hover:subtle-border transition"
     aria-label="Scroll to top"
   >
-    <img src="@/assets/market/goToTop.svg" alt="Go to top" class="w-8 h-8" />
+    <img src="@/assets/market/goToTop.svg" alt="Go to top" class="w-8 h-8 bg-opacity-70" />
   </button>
 </template>
 
