4ed35b43b7021037a23086d0ac0795299a068209
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-13 11:15:50 +0900

    [feat] ReportAlert: ai 기반 활동 리포트 컴포넌트 추가

---
+++ src/components/influencerMypage/statistics/ReportAlert.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Report from '@/assets/InfluencerProfile/Report.svg'
+</script>
+<template>
+  <div class="w-full rounded-xl flex flex-col bg-white p-4">
+    <div class="flex gap-1 items-center">
+        <img :src="Report" class="w-6 h-6"></img>
+        <p class="text-md font-semibold">AI 기반 활동 리포트가 준비됐어요</p>
+    </div>
+
+    <p class="text-sm font-medium mt-1">지난주 패턴을 바탕으로 다음 주 행동 가이드를 만들었어요.</p>
+    <button class="bg-brand-primary text-white rounded-lg mt-3 w-24 mx-auto p-1"> 리포트 열기</button>
+  </div>
+</template>
