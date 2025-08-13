b25179147cc4b524ee535dba07007678d9da6bc1
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-13 11:05:50 +0900

    [style] InfluencerProfile: 피그마 변경 디자인 적용

--- src/components/influencerMypage/InfluencerProfile.vue
+++ src/components/influencerMypage/InfluencerProfile.vue
@@ -6,14 +6,17 @@ const imageUrl = profile
 </script>
 
 <template>
-  <div class="flex items-start gap-4 bg-white p-5 mt-10">
+  <div class="flex items-start gap-4 bg-white py-8 ps-8 mt-[48px] rounded-xl">
     <img
       :src="imageUrl"
       :alt="`${name} 프로필 이미지`"
-      class="w-20 h-20 rounded-full object-cover"
+      class="w-14 h-14 rounded-full object-cover"
     />
     <div class="flex flex-col justify-center">
-      <p class="text-lg font-semibold">{{ name }}님, 안녕하세요!</p>
+      <p class="text-xl font-bold">{{ name }}님, 안녕하세요!</p>
+      <p class="text-xs font-semibold text-subtle-text mt-1">
+        아래에서 AI 분석 보고서를 확인하실 수 있어요!
+      </p>
     </div>
   </div>
 </template>
