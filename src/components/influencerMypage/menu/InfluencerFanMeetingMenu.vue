81e9b417c897d9036961f169bbe70b9e420d30ae
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-13 11:04:32 +0900

    [style] InfluencerFanMeetingMenu: border-t 삭제

--- src/components/influencerMypage/menu/InfluencerFanMeetingMenu.vue
+++ src/components/influencerMypage/menu/InfluencerFanMeetingMenu.vue
@@ -10,7 +10,6 @@ import check from '@/assets/mypage/BsCheckCircle-1.svg'
 //   router.push('/') // 예매 확인 컴포넌트로
 // }
 
-
 const items = [
   { icon: ticket, label: '팬미팅 정보 추가' },
   { icon: check, label: '예매 확인' },
@@ -18,11 +17,11 @@ const items = [
 </script>
 
 <template>
-  <div class="border-t">
+  <div>
     <div class="px-5 pt-5">
       <p class="text-xl font-bold text-black">나의 팬미팅 정보</p>
     </div>
-    <div v-for="(item, index) in items" :key="index" class="flex items-center gap-3 px-5 py-3">
+    <div v-for="(item, index) in items" :key="index" class="flex items-center gap-2 px-5 py-3">
       <img :src="item.icon" alt="아이콘" class="w-5 h-5 cursor-pointer" />
       <span class="text-base font-medium text-black cursor-pointer">{{ item.label }}</span>
     </div>
