2e49b661c3ca5b0676d4d249046af039d372710b
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-13 11:07:29 +0900

    [feat] InfluencerMenu: 메뉴 컴포넌트 종합 파일 추가

---
+++ src/components/influencerMypage/menu/InfluencerMenu.vue
@@ -0,0 +1,14 @@
+<script setup>
+import InfluencerAccountMenu from './menu/InfluencerAccountMenu.vue'
+import InfluencerMarketMenu from './menu/InfluencerMarketMenu.vue'
+import InfluencerFanMeetingMenu from './menu/InfluencerFanMeetingMenu.vue'
+</script>
+<template>
+  <div class="w-full rounded-xl flex flex-col bg-white py-2">
+    <InfluencerAccountMenu />
+    <hr />
+    <InfluencerMarketMenu />
+    <hr />
+    <InfluencerFanMeetingMenu />
+  </div>
+</template>
