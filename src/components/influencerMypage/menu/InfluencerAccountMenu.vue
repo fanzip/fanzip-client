27cae81d1bfffa7c6204f5e062896ee960b9952b
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-13 11:03:54 +0900

    [fix] InfluencerAccountMenu: 멤버십 관리 삭제

--- src/components/influencerMypage/menu/InfluencerAccountMenu.vue
+++ src/components/influencerMypage/menu/InfluencerAccountMenu.vue
@@ -14,18 +14,17 @@ const menuItems = [
     label: '프로필 관리',
     action: () => router.push('/influencer/profile'),
   },
-  { icon: Card, label: '멤버십 관리' },
   { icon: Logout, label: '로그아웃' },
   { icon: SadFace, label: '회원탈퇴' },
 ]
 </script>
 
 <template>
-  <div class="border-t">
+  <div>
     <div
       v-for="(item, index) in menuItems"
       :key="index"
-      class="flex items-center gap-3 px-5 py-3 cursor-pointer"
+      class="flex items-center gap-2 px-5 py-3 cursor-pointer"
       @click="item.action"
     >
       <img :src="item.icon" alt="아이콘" class="w-5 h-5" />
