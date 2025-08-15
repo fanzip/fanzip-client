5277ee8a89c04bee532e6a3d2862a2fd295ccf8f
AshtonSW <sprauncy76@gmail.com>
2025-08-16 02:44:43 +0900

    [feat] VitePwa 추가

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
