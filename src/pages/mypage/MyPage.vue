a87856626a35069f9b618f5bb4f270045ec75024
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-17 02:02:45 +0900

    [feat] 마이페이지 디자인 수정, 최고등급/보유 카드 api 연동

--- src/pages/mypage/MyPage.vue
+++ src/pages/mypage/MyPage.vue
@@ -1,23 +1,39 @@
 <script setup>
 import AppHeader from '@/components/layout/AppHeader.vue'
 import ListItem from '@/components/mypage/ListItem.vue'
+import lockIcon from '@/assets/mypage/Lock.svg'
+import cardIcon from '@/assets/mypage/Card.svg'
+import shoppingBagIcon from '@/assets/mypage/ShoppingBag.svg'
+import truckIcon from '@/assets/mypage/Truck.svg'
+import logoutIcon from '@/assets/mypage/Logout.svg'
+import sadFaceIcon from '@/assets/mypage/SadFace.svg'
 import { useAuthStore } from '@/stores/authStore'
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import userApi from '@/api/userApi'
 import AppNav from '@/components/layout/AppNav.vue'
-import BaseButton from '@/components/common/BaseButton.vue'
 import router from '@/router'
+import { getMyMembershipInfo } from '@/api/membershipApi'
+import { fancardApi } from '@/api/fancardApi'
 
 const authStore = useAuthStore()
 const logout = authStore.logout
 const userName = computed(() => authStore.userInfo.name)
 const role = computed(() => authStore.userInfo.role)
+const highestGrade = ref(null)
+const cards = ref(0)
+
 onMounted(async () => {
   const token = authStore.token
   if (token) {
     try {
       const res = await userApi.getUserInfo()
       authStore.setUserInfo(res.data)
+
+      const res2 = await getMyMembershipInfo()
+      highestGrade.value = res2.userGrade
+
+      const response = await fancardApi.getUserFancards()
+      cards.value = response.data.fancards.length
     } catch (e) {
       console.error(e)
     }
@@ -25,75 +41,92 @@ onMounted(async () => {
 })
 </script>
 <template>
-  <div class="w-full flex flex-col">
-    <AppHeader type="logo" />
-    <div class="mt-12 flex justify-between">
-      <div>
-        <p class="text-xl font-bold ms-5">{{ userName }}님, 안녕하세요!</p>
-        <div class="flex justify-between ms-5 w-32">
-          <p class="text-xl">최고등급</p>
-          <p class="text-xl font-bold">GOLD</p>
+  <AppHeader type="logo" />
+  <div class="p-5 pb-20 min-h-screen bg-subtle-bg space-y-4">
+    <!-- 프로필 -->
+    <section class="flex items-start gap-4 bg-white p-5 mt-[48px] rounded-xl">
+      <div class="flex flex-col justify-center">
+        <p class="text-xl font-bold">{{ userName }}님, 안녕하세요!</p>
+        <div class="flex items-center text-base">
+          <p>최고등급</p>
+          <p class="font-semibold ms-1">{{ highestGrade }}</p>
         </div>
       </div>
-      <BaseButton
+      <button
         v-if="role === 'INFLUENCER'"
-        variant="buy"
-        size="sm"
+        class="bg-brand-primary"
         @click.prevent="router.push({ name: 'influencers-mypage' })"
-        >인플루언서 페이지로 이동</BaseButton
       >
-    </div>
-    <div class="border border-subtle-border mt-6"></div>
-    <div class="mt-4 mb-4">
-      <p class="font-bold ms-5">나의 참여</p>
-      <div class="flex justify-between text-center bg-subtle-bg rounded-lg mx-5 mt-2 p-2">
-        <div class="flex-1">
-          <p class="font-semibold text-sm">보유 팬 카드 수</p>
-          <p class="font-bold mt-1">9</p>
-        </div>
-        <div class="w-px bg-gray-300 mx-2"></div>
-        <div class="flex-1">
-          <p class="font-semibold text-sm">팬미팅 참여 횟수</p>
-          <p class="font-bold mt-1">3</p>
-        </div>
-        <div class="w-px bg-gray-300 mx-2"></div>
-        <div class="flex-1">
-          <p class="font-semibold text-sm">공동 구매 횟수</p>
-          <p class="font-bold mt-1">7</p>
-        </div>
+        토글
+      </button>
+    </section>
+
+    <section class="gap-4 flex">
+      <div class="flex-1 rounded-xl p-4 flex flex-col items-center bg-white space-y-1">
+        <p class="text-lg">🔥</p>
+        <p class="font-semibold text-sm">보유 팬 카드</p>
+        <p class="font-bold mt-1 text-lg">{{ cards }}장</p>
+      </div>
+
+      <div class="flex-1 rounded-xl p-4 flex flex-col items-center bg-white space-y-1">
+        <p class="text-lg">💫</p>
+        <p class="font-semibold text-sm">팬미팅 참여</p>
+        <p class="font-bold mt-1 text-lg">3회</p>
+      </div>
+
+      <div class="flex-1 rounded-xl p-4 flex flex-col items-center bg-white space-y-1">
+        <p class="text-lg">🛍️</p>
+        <p class="font-semibold text-sm">공동 구매</p>
+        <p class="font-bold mt-1 text-lg">7회</p>
+      </div>
+    </section>
+
+    <!--   <div class="gap-4 flex">
+    <div class="w-1/2 rounded-xl p-4 flex flex-col items-center bg-white space-y-1">
+      <p class="font-semibold">현재 구독자</p>
+      <p class="font-semibold">{{ currentSubscribers.toLocaleString() }}명</p>
+      <div class="flex gap-2 text-xs font-medium">
+        <p>Today:</p>
+        <img :src="Increase" />
+        <p>{{ todaySubscriberGrowth.toLocaleString() }}명</p>
       </div>
     </div>
-    <div class="border border-subtle-border"></div>
-    <div class="mb-16">
+    <div class="w-1/2 rounded-xl p-4 flex flex-col items-center bg-white space-y-1">
+      <p class="font-semibold">누적 수익</p>
+      <p class="font-semibold">{{ totalRevenue.toLocaleString() }}원</p>
+      <div class="flex gap-2 text-xs font-medium">
+        <p class="font-medium">Today:</p>
+        <img :src="Decrease" />
+        <p>{{ todayRevenueGrowth.toLocaleString() }}원</p>
+      </div>
+    </div>
+  </div> -->
+
+    <div class="gap-4 bg-white pt-2 p-4 rounded-xl">
       <router-link to="/account">
-        <ListItem src="/src/assets/mypage/Lock.svg" menu="계정 관리" gap="2"></ListItem
+        <ListItem :src="lockIcon" menu="프로필 관리" gap="2"></ListItem
       ></router-link>
       <router-link to="/fancard">
-        <ListItem src="/src/assets/mypage/Card.svg" menu="멤버십 관리" gap="2"></ListItem>
+        <ListItem :src="cardIcon" menu="멤버십 관리" gap="2"></ListItem>
       </router-link>
+      <router-link to="/shipping">
+        <ListItem :src="truckIcon" menu="배송지 관리" gap="2"></ListItem>
+      </router-link>
+
       <router-link to="/my-payments">
-        <ListItem src="/src/assets/mypage/ShoppingBag.svg" menu="결제 내역" gap="2"></ListItem
+        <ListItem :src="shoppingBagIcon" menu="결제 내역" gap="2"></ListItem
       ></router-link>
 
-      <router-link to="/shipping">
-        <ListItem src="/src/assets/mypage/Truck.svg" menu="배송지 관리" gap="2"></ListItem>
-      </router-link>
-
-      <ListItem
-        src="/src/assets/mypage/Logout.svg"
-        menu="로그아웃"
-        @click="logout"
-        gap="2"
-      ></ListItem>
-      <ListItem src="/src/assets/mypage/SadFace.svg" menu="회원탈퇴" gap="3"></ListItem>
+      <ListItem :src="logoutIcon" menu="로그아웃" @click="logout" gap="2"></ListItem>
+      <ListItem :src="sadFaceIcon" menu="회원탈퇴" gap="3"></ListItem>
     </div>
 
-    <div class="bg-subtle-bg pt-3 px-5 pb-20">
+    <div class="bg-subtle-bg py-3">
       <div class="text-xs flex gap-5">
         <p>이용약관</p>
         <p>개인정보 처리방침</p>
       </div>
-      <div class="text-sm text-subtle-text mt-24 mb-0">
+      <div class="text-sm text-subtle-text mt-6 mb-0">
         <p>COPYRIGHT © (주)좋은데요</p>
         <p>ALL RIGHTS RESERVED.</p>
       </div>
