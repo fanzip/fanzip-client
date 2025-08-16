ba32cbaef437f9669ba4f2b6246d9324b087cea2
dayeon1211 <dayeon1211m@gmail.com>
2025-08-16 23:54:57 +0900

    [fix] 마켓 화면 헤더와 네비게이션때문에 잘리던 문제 해결 #116

--- src/pages/market/MarketDetailPage.vue
+++ src/pages/market/MarketDetailPage.vue
@@ -112,129 +112,130 @@ function goToOrder() {
 <template>
   <!-- 헤더 -->
   <AppHeader type="back-icons" />
+  <div class="pt-12">
+    <!-- 품절 & 오픈전 오버레이-->
+    <div
+      v-if="product.stock === 0 || !product.available"
+      class="fixed top-12 left-0 right-0 bottom-0 bg-black bg-opacity-30 z-10 pointer-events-none"
+    ></div>
 
-  <!-- 품절 & 오픈전 오버레이-->
-  <div
-    v-if="product.stock === 0 || !product.available"
-    class="fixed top-12 left-0 right-0 bottom-0 bg-black bg-opacity-30 z-10 pointer-events-none"
-  ></div>
-
-  <!-- 슬라이드 이미지 -->
-  <div class="w-full aspect-square overflow-hidden relative">
-    <img
-      :src="product.detailImagesList[currentSlide]"
-      class="w-full h-full object-cover transition-all duration-500"
-      alt="상세사진 - 슬라이드"
-    />
-  </div>
-
-  <!-- 상품 정보 -->
-  <div class="ml-5 mr-5 space-y-2">
-    <!-- 상품명 -->
-    <p class="text-xl font-medium">{{ product.name }}</p>
-
-    <!-- 할인율 + 할인전가격-->
-    <div class="flex space-x-2">
-      <div class="text-xl font-bold text-subtle-text">
-        {{ Math.round(product.discountRate * 100) }}%
-      </div>
-      <div class="font-medium line-through text-xl text-subtle-border">{{ product.price }}원</div>
-    </div>
-
-    <!-- 할인가 + 배송비 -->
-    <div class="flex items-center justify-between">
-      <div class="flex items-end space-x-2">
-        <div class="text-2xl font-extrabold text-text-emphasis">
-          {{ product.discountedPrice }}원
-        </div>
-        <div class="text-base font-semibold text-text-emphasis">나의 할인가</div>
-      </div>
-
-      <div class="text-base font-semibold text-subtle-text flex justify-end space-x-2 items-end">
-        <img src="@/assets/market/BsTruck.svg" alt="Truck logo" />
-        <span v-if="product.shippingPrice === 0">무료배송</span>
-        <span v-else>{{ product.shippingPrice.toLocaleString() }}원</span>
-      </div>
-    </div>
-
-    <!-- 구분선 -->
-    <div>
-      <hr class="border-t border-nav-deactivated w-full mt-6" />
-    </div>
-
-    <!-- 설명 이미지 -->
-    <div class="mt-4 space-y-4">
+    <!-- 슬라이드 이미지 -->
+    <div class="w-full aspect-square overflow-hidden relative">
       <img
-        v-for="(img, i) in product.descriptionImagesList"
-        :key="i"
-        :src="img"
-        class="w-full rounded-lg"
-        alt="description"
+        :src="product.detailImagesList[currentSlide]"
+        class="w-full h-full object-cover transition-all duration-500"
+        alt="상세사진 - 슬라이드"
       />
     </div>
 
-    <!-- 1. 품절인 경우 -->
-    <div v-if="product.stock === 0">
-      <nav
-        class="fixed bottom-0 left-0 w-full h-20 bg-base-bg border-t border-nav-stroke rounded-t-2xl flex items-center justify-center z-50"
-      >
-        <div
-          class="w-[353px] h-12 bg-subtle-border rounded-xl font-bold text-base flex items-center justify-center"
+    <!-- 상품 정보 -->
+    <div class="ml-5 mr-5 space-y-2">
+      <!-- 상품명 -->
+      <p class="text-xl font-medium">{{ product.name }}</p>
+
+      <!-- 할인율 + 할인전가격-->
+      <div class="flex space-x-2">
+        <div class="text-xl font-bold text-subtle-text">
+          {{ Math.round(product.discountRate * 100) }}%
+        </div>
+        <div class="font-medium line-through text-xl text-subtle-border">{{ product.price }}원</div>
+      </div>
+
+      <!-- 할인가 + 배송비 -->
+      <div class="flex items-center justify-between">
+        <div class="flex items-end space-x-2">
+          <div class="text-2xl font-extrabold text-text-emphasis">
+            {{ product.discountedPrice }}원
+          </div>
+          <div class="text-base font-semibold text-text-emphasis">나의 할인가</div>
+        </div>
+
+        <div class="text-base font-semibold text-subtle-text flex justify-end space-x-2 items-end">
+          <img src="@/assets/market/BsTruck.svg" alt="Truck logo" />
+          <span v-if="product.shippingPrice === 0">무료배송</span>
+          <span v-else>{{ product.shippingPrice.toLocaleString() }}원</span>
+        </div>
+      </div>
+
+      <!-- 구분선 -->
+      <div>
+        <hr class="border-t border-nav-deactivated w-full mt-6" />
+      </div>
+
+      <!-- 설명 이미지 -->
+      <div class="mt-4 space-y-4 pb-28">
+        <img
+          v-for="(img, i) in product.descriptionImagesList"
+          :key="i"
+          :src="img"
+          class="w-full rounded-lg"
+          alt="description"
+        />
+      </div>
+
+      <!-- 1. 품절인 경우 -->
+      <div v-if="product.stock === 0">
+        <nav
+          class="fixed bottom-0 left-0 w-full h-20 bg-base-bg border-t border-nav-stroke rounded-t-2xl flex items-center justify-center z-50"
         >
-          품절
-        </div>
-      </nav>
-    </div>
+          <div
+            class="w-[353px] h-12 bg-subtle-border rounded-xl font-bold text-base flex items-center justify-center"
+          >
+            품절
+          </div>
+        </nav>
+      </div>
 
-    <!-- 2. 오픈 시간이 안된 경우 -->
-    <div v-else-if="!product.available">
-      <nav
-        class="fixed bottom-0 left-0 w-full h-28 bg-base-bg border-t border-nav-stroke rounded-t-2xl flex items-center justify-center z-50"
-      >
-        <div
-          class="w-full h-16 ml-5 mr-5 bg-subtle-border rounded-xl text-base flex flex-col items-center justify-center text-center leading-none font-semibold"
+      <!-- 2. 오픈 시간이 안된 경우 -->
+      <div v-else-if="!product.available">
+        <nav
+          class="fixed bottom-0 left-0 w-full h-28 bg-base-bg border-t border-nav-stroke rounded-t-2xl flex items-center justify-center z-50"
         >
-          <span>
-            <span class="font-extrabold">{{ getGradeName(product.gradeId) }}</span> 등급의 오픈
-            시간은
-          </span>
-          <span>
-            <span class="font-extrabold">{{ formatOpenTime(product.openTime) }}</span> 입니다.
-          </span>
-        </div>
-      </nav>
-    </div>
+          <div
+            class="w-full h-16 ml-5 mr-5 bg-subtle-border rounded-xl text-base flex flex-col items-center justify-center text-center leading-none font-semibold"
+          >
+            <span>
+              <span class="font-extrabold">{{ getGradeName(product.gradeId) }}</span> 등급의 오픈
+              시간은
+            </span>
+            <span>
+              <span class="font-extrabold">{{ formatOpenTime(product.openTime) }}</span> 입니다.
+            </span>
+          </div>
+        </nav>
+      </div>
 
-    <!-- 3. 정상 -->
-    <div v-else>
-      <nav
-        class="fixed bottom-0 left-0 w-full h-20 bg-base-bg border-t border-nav-stroke rounded-t-2xl flex items-center justify-center z-50"
-      >
-        <!-- 3-1. 장바구니 담기 전  -->
-        <div v-if="!cartAdded" class="flex gap-4 items-center">
-          <BaseButton variant="cart" size="md" @click="addToCart">
-            <img src="@/assets/button/shopping-bag.svg" alt="장바구니" class="w-5 h-5" />
-            장바구니 담기
-          </BaseButton>
-          <BaseButton variant="buy" size="md" @click="goToOrder">바로 구매하기</BaseButton>
-        </div>
-
-        <!-- 3-2. 장바구니 담은 후 -->
-        <div v-else class="flex justify-between items-center px-6 w-full">
-          <div class="flex items-center gap-1">
-            <img src="@/assets/market/shoppingBagGrey.svg" alt="장바구니2" class="w-5 h-5" />
-            <span class="text-base font-bold"> 장바구니에 상품을 담았습니다 </span>
+      <!-- 3. 정상 -->
+      <div v-else>
+        <nav
+          class="fixed bottom-0 left-0 w-full h-20 bg-base-bg border-t border-nav-stroke rounded-t-2xl flex items-center justify-center z-50"
+        >
+          <!-- 3-1. 장바구니 담기 전  -->
+          <div v-if="!cartAdded" class="flex gap-4 items-center">
+            <BaseButton variant="cart" size="md" @click="addToCart">
+              <img src="@/assets/button/shopping-bag.svg" alt="장바구니" class="w-5 h-5" />
+              장바구니 담기
+            </BaseButton>
+            <BaseButton variant="buy" size="md" @click="goToOrder">바로 구매하기</BaseButton>
           </div>
 
-          <button
-            @click="goToCart"
-            class="flex items-center gap-0 text-subtle-text text-base font-medium"
-          >
-            <span>바로가기</span>
-            <img src="@/assets/market/chevron-down.svg" alt=">" />
-          </button>
-        </div>
-      </nav>
+          <!-- 3-2. 장바구니 담은 후 -->
+          <div v-else class="flex justify-between items-center px-6 w-full">
+            <div class="flex items-center gap-1">
+              <img src="@/assets/market/shoppingBagGrey.svg" alt="장바구니2" class="w-5 h-5" />
+              <span class="text-base font-bold"> 장바구니에 상품을 담았습니다 </span>
+            </div>
+
+            <button
+              @click="goToCart"
+              class="flex items-center gap-0 text-subtle-text text-base font-medium"
+            >
+              <span>바로가기</span>
+              <img src="@/assets/market/chevron-down.svg" alt=">" />
+            </button>
+          </div>
+        </nav>
+      </div>
     </div>
   </div>
 </template>
