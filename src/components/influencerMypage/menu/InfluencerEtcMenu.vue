c050c466085e9cde28198950cd297dbb09a83d52
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-17 13:50:03 +0900

    [chore] 인플루언서 마이페이지 로그아웃 활성화

--- src/components/influencerMypage/menu/InfluencerEtcMenu.vue
+++ src/components/influencerMypage/menu/InfluencerEtcMenu.vue
@@ -1,8 +1,11 @@
 <script setup>
 import Logout from '@/assets/mypage/Logout.svg'
 import SadFace from '@/assets/mypage/SadFace.svg'
+import { useAuthStore } from '@/stores/authStore'
+
+const logout = useAuthStore().logout
 const menuItems = [
-  { icon: Logout, label: '로그아웃' },
+  { icon: Logout, label: '로그아웃', action: logout },
   { icon: SadFace, label: '회원탈퇴' },
 ]
 </script>
@@ -16,7 +19,7 @@ const menuItems = [
       @click="item.action"
     >
       <img :src="item.icon" alt="아이콘" class="w-5 h-5" />
-      <span class="text-base font-medium text-black">{{ item.label }}</span>
+      <span class="text-base font-semibold text-black">{{ item.label }}</span>
     </div>
   </div>
 </template>
